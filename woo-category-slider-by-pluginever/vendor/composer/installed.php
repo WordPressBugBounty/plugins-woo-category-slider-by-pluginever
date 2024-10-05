9a1c971b792c2e5f8ed001c556014c6cfa4fc025
Sean Murphy <sean@iamseanmurphy.com>
2024-10-05 18:50:43 +0000

    4.2.5

---
+++ woo-category-slider-by-pluginever/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wc-category-slider',
+        'pretty_version' => 'v4.2.5',
+        'version' => '4.2.5.0',
+        'reference' => '6d92675cdcd2388cc2b2d9813ac2a860995a63a0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pluginever/wc-category-slider' => array(
+            'pretty_version' => 'v4.2.5',
+            'version' => '4.2.5.0',
+            'reference' => '6d92675cdcd2388cc2b2d9813ac2a860995a63a0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
